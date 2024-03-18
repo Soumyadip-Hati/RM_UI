af9e9955e732e086d6221370522090c2dd64954d
Soumyadip-Hati <soumyadip.hati@alumnux.com>
2024-03-18 19:29:07 +0530

    5th Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,18 +21,25 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <head>
         <link rel="icon" href="/images/favicon.webp" type="image/x-icon" sizes="20x20" />
       </head>
-      <body className={roboto_slab.className}>
-        <nav className="flex items-center">
+      <body className={`${roboto_slab.className} bg-gray-100`}>
+        <section className="flex">
           <Image src="/images/logo_1.webp" alt="logo" width={200} height={100} priority />
-          <div className="w-full align-middle p-5">
-            <ul className="flex gap-3 m-auto">
-              <li><Link href="/home">Home</Link></li>
-              <li><Link href="/about">About</Link></li>
-            </ul>
-          </div>
-        </nav>
-        <hr />
-        {children}
+          <nav className="bg-gray-500 w-full">
+            <div className="grid grid-cols-1 h-full">
+            <div className="flex flex-col font-bold text-lg text-slate-200 items-center h-full"><span className="align-middle">Release Manager</span></div>
+            <div className="flex flex-col w-full h-full">
+              <ul className="flex gap-3 font-semibold text-slate-50">
+                <li className="border-solid border-2 border-slate-100"><Link href="/home">Home</Link></li>
+                <li><Link href="/about">About</Link></li>
+              </ul>
+            </div>
+            </div>
+          </nav>
+        </section>
+        <hr className="bg-slate-100" />
+        <section className="py-7 px-5">
+          {children}
+        </section>
       </body>
     </html>
   );
