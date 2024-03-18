563f458359822ae87e1372988679fb4895c183b5
Soumyadip-Hati <soumyadip.hati@alumnux.com>
2024-03-18 21:05:19 +0530

    7th Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           <Image src="/images/logo_1.webp" alt="logo" width={200} height={100} priority />
           <nav className="bg-gray-500 w-full">
             <div className="grid grid-cols-1 h-full">
-            <div className="flex flex-col font-bold text-lg text-slate-200 items-start justify-center px-3 h-full"><span className="align-middle">Release Manager</span></div>
+            <div className="flex flex-col font-bold text-2xl text-slate-200 items-start justify-center px-3 h-full"><span className="align-middle">Release Manager</span></div>
             <div className="flex flex-col w-full h-full items-start justify-end">
               <ul className="flex gap-3 font-semibold text-slate-50 h-full pt-3">
                 <li className="flex border-solid border-2 border-slate-100 px-3"><Link className="m-auto" href="/home">Home</Link></li>
